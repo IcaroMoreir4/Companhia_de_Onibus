443feb2138334f65550acf80d4e15d5e597c0eda
IcaroMoreir4 <icaromoreira90@gmail.com>
2024-05-06 20:35:39 -0300

    commit

---
+++ companhia_de_onibus.sql
@@ -0,0 +1,176 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Tempo de geração: 07/05/2024 às 01:33
+-- Versão do servidor: 10.4.32-MariaDB
+-- Versão do PHP: 8.0.30
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Banco de dados: `companhia_de_onibus`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `motorista`
+--
+
+CREATE TABLE `motorista` (
+  `motorista_id` int(11) NOT NULL,
+  `nome` varchar(255) NOT NULL,
+  `cnh` int(11) NOT NULL,
+  `cpf` int(11) NOT NULL,
+  `salario` float NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `onibus`
+--
+
+CREATE TABLE `onibus` (
+  `onibus_id` int(11) NOT NULL,
+  `estado` varchar(255) NOT NULL,
+  `capacidade` int(11) NOT NULL,
+  `placa` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `passageiro`
+--
+
+CREATE TABLE `passageiro` (
+  `passageiro_id` int(11) NOT NULL,
+  `nome` varchar(255) NOT NULL,
+  `cpf` int(11) NOT NULL,
+  `telefone` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `passagem`
+--
+
+CREATE TABLE `passagem` (
+  `passagem_id` int(11) NOT NULL,
+  `passageiro_id` int(11) NOT NULL,
+  `rota_id` int(11) NOT NULL,
+  `data_viagem` date NOT NULL,
+  `preco` float NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `rota`
+--
+
+CREATE TABLE `rota` (
+  `rota_id` int(11) NOT NULL,
+  `origem` varchar(100) NOT NULL,
+  `destino` varchar(100) NOT NULL,
+  `distancia` float NOT NULL,
+  `duracao_estimada` time NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Índices para tabelas despejadas
+--
+
+--
+-- Índices de tabela `motorista`
+--
+ALTER TABLE `motorista`
+  ADD PRIMARY KEY (`motorista_id`);
+
+--
+-- Índices de tabela `onibus`
+--
+ALTER TABLE `onibus`
+  ADD PRIMARY KEY (`onibus_id`);
+
+--
+-- Índices de tabela `passageiro`
+--
+ALTER TABLE `passageiro`
+  ADD PRIMARY KEY (`passageiro_id`);
+
+--
+-- Índices de tabela `passagem`
+--
+ALTER TABLE `passagem`
+  ADD PRIMARY KEY (`passagem_id`),
+  ADD KEY `passageiro_id` (`passageiro_id`),
+  ADD KEY `rota_id` (`rota_id`);
+
+--
+-- Índices de tabela `rota`
+--
+ALTER TABLE `rota`
+  ADD PRIMARY KEY (`rota_id`);
+
+--
+-- AUTO_INCREMENT para tabelas despejadas
+--
+
+--
+-- AUTO_INCREMENT de tabela `motorista`
+--
+ALTER TABLE `motorista`
+  MODIFY `motorista_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de tabela `onibus`
+--
+ALTER TABLE `onibus`
+  MODIFY `onibus_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de tabela `passageiro`
+--
+ALTER TABLE `passageiro`
+  MODIFY `passageiro_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de tabela `passagem`
+--
+ALTER TABLE `passagem`
+  MODIFY `passagem_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de tabela `rota`
+--
+ALTER TABLE `rota`
+  MODIFY `rota_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Restrições para tabelas despejadas
+--
+
+--
+-- Restrições para tabelas `passagem`
+--
+ALTER TABLE `passagem`
+  ADD CONSTRAINT `passagem_ibfk_1` FOREIGN KEY (`passageiro_id`) REFERENCES `passageiro` (`passageiro_id`),
+  ADD CONSTRAINT `passagem_ibfk_2` FOREIGN KEY (`rota_id`) REFERENCES `rota` (`rota_id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
